207896d607f14b65d2b77d40791c27b450d2bd59
Dieonx <dieonnwani18@gmail.com>
2024-08-29 16:53:37 +0100

    apply

--- app/about/page.js
+++ app/about/page.js
@@ -5,7 +5,7 @@ const AboutPage = () => {
     <div className='p-7 sp-y-4 space-x-5'>
     <p className='px-4 py-8'>Hi my Name is Ebube Nwani, and i am a web developer. i am currently studing software development in Godfrey Okoye University 
       locate in Enugu. I am in my 200 Level, I love software because i was exposed to
-       devices and technology at a young age which i seeme to always find very fun and intresting 
+       devices and technology at a young age which i seeme to always find very fun and intresting,
       software as the name may implie is very fun but also very technical.    </p>
        <p className='px-4 py-2'>I am the fisrt child of my parents and the first son of my family. i am 20 years old and still have a long way to go in order for me to become a professional software developer and i am ready for the challenges and tasks up ahead.
          </p>
