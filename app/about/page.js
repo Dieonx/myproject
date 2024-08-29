236665c77209b448bce53dfc038a85a483b0eff7
Dieonx <dieonnwani18@gmail.com>
2024-08-29 16:50:56 +0100

    app

--- app/about/page.js
+++ app/about/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 const AboutPage = () => {
   return (
     <div className='p-7 sp-y-4 space-x-5'>
-    <p className='px-4 py-8'>Hi my Name is Ebube Nwani and i am a web developer. i am currently studing software development in Godfrey Okoye University 
+    <p className='px-4 py-8'>Hi my Name is Ebube Nwani, and i am a web developer. i am currently studing software development in Godfrey Okoye University 
       locate in Enugu. I am in my 200 Level, I love software because i was exposed to
        devices and technology at a young age which i seeme to always find very fun and intresting 
       software as the name may implie is very fun but also very technical.    </p>
